7f41c0e6adf2832ae8a311dc126e1247be81cf20
Cliff Jean <cliff.lilly.jean@gmail.com>
2026-04-15 11:52:46 -0400

    working on getting the keyboard to detect ctrl-d

---
+++ adieu.py
@@ -0,0 +1,18 @@
+def main():
+
+    import inflect
+    import keyboard
+
+    # ask the user for names, one per line until control-d is input
+
+
+    while True:
+        if keyboard.is_pressed("a"):
+            print("pressed v")
+            break
+
+
+
+
+
+main()
