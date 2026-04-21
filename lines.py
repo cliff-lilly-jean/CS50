4e9b3cd0ec3582dba4049933a5607e4812a15a6d
Cliff Jean <cliff.lilly.jean@gmail.com>
2026-04-21 18:24:58 -0400

    added comments to the lines program

---
+++ lines.py
@@ -0,0 +1,11 @@
+def main():
+
+    import sys
+
+    # check for #,  skip
+    # check for whitespace, skip
+    # check the file ends in .py, selse ys.exit()
+    # check the file exists, else sys.exit()
+    # check for 1 command line argument, else sys.exit()
+
+main()
