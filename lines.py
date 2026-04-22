a8ae397a33107b658eadaa34458e47a7eaec9453
Cliff Jean <cliff.lilly.jean@gmail.com>
2026-04-22 15:12:49 -0400

    updated the frist 2 if statements in the program

--- lines.py
+++ lines.py
@@ -2,10 +2,29 @@ def main():
 
     import sys
 
+    # check for 1 command line argument, else sys.exit()
+    if sys.argv[2:]:
+        print("too many arguments")
+        sys.exit()
+    else:
+        print("good")
+
+    # check the file ends in .py, else sys.exit()
+    if sys.argv[1].endswith(".py"):
+        print("Correct format")
+    else:
+        sys.exit()
+
+
+     # check the file exists, else sys.exit()
+
     # check for #,  skip
     # check for whitespace, skip
-    # check the file ends in .py, selse ys.exit()
-    # check the file exists, else sys.exit()
-    # check for 1 command line argument, else sys.exit()
+
+
+
+
+
+
 
 main()
